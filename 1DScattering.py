7ddc64cf07cf94a11b363cd43582375a154fc77c
August Herron <66635096+augustryder@users.noreply.github.com>
2024-12-05 21:54:13 -0600

    cleanup

--- 1DScattering.py
+++ 1DScattering.py
@@ -6,69 +6,89 @@ from scipy.interpolate import BSpline
 
 m = 1
 hbar = 1
+E = 2
 
 a = 4 # Gaussian stdev
 # Gaussian potential
 def V(x):
-    V0 = 10
+    V0 = 8
     return -V0 * np.exp(-x**2 / (2 * a**2))
 
 x_m = 4 * a # Boundry 
 
 # Order
 k = 5
-# Number of basis functions
-N = x_m + k
 
+# Number of basis functions
+N = 50
+assert(N > k)
+
+# number of points on x_grid
+xdim = N - k + 1
+
+xl = 0
+xr = x_m
+
+x_grid = [xl + (xr - xl) * n / (xdim-1) for n in range(0, xdim)]
+print(x_grid)
+
+# beta = -logder 
+# B1' = -B2'
+# {c1B1 + c2B2, B3, ..., Bxdim +k -1}
 # Knot set
 t = []
 for _ in range(k):
-    t.append(0)
-for i in range(N - k + 1):
-    t.append(i)
+    t.append(x_grid[0])
+t += x_grid
 for _ in range(k):
-    t.append(N - k)
-    
+    t.append(x_grid[xdim-1])
+
+print(t)
+print(len(t))
+
 # Coefficient array
-c = [0 for _ in range(N)]
+c = [0 for _ in range(N+1)]
 
 # discrete x-axis
 x_inner = np.linspace(0, x_m, 2**10 + 1)
 x_outer = np.linspace(x_m, 2*x_m, 2**10 + 1)
 x_full = np.linspace(-2, 2*x_m, 2**10 + 1)
 
-plt.figure(figsize=(9, 9))
-
-
 # x array, order, id, knot set, coeff arr
-def B(x, k, i, t, c):
-   c[i] = 1
-   spl = BSpline(t, c, k)
-   c[i] = 0
-   return spl(x)
+def B(i, x):
+        c[i] = 1
+        spl = BSpline(t, c, k)
+        c[i] = 0
+        return spl(x)
 
-def dB(x, k, i, t, c):
-   c[i] = 1
-   spl = BSpline(t, c, k).derivative()
-   c[i] = 0
-   return spl(x)
+def dB(i, x):
+        c[i] = 1
+        spl = BSpline(t, c, k).derivative()
+        c[i] = 0
+        return spl(x)
 
+# y_0 = 0.4
+# basis = [ y_0 * (B(0, x_inner) + B(1, x_inner)) ] + [ B(i, x_inner) for i in range(2, N+1)]
+# dbasis = [ y_0 * (dB(0, x_inner) + dB(1, x_inner)) ] + [ dB(i, x_inner) for i in range(2, N+1)]
+
+basis = [ B(i, x_inner) for i in range(0, N)]
+dbasis = [ dB(i, x_inner) for i in range(0, N)]
+
+plt.figure(figsize=(9, 9))
 for i in range(N):
-   plt.plot(x_inner, B(x_inner, k, i, t, c), alpha=0.1)
+   plt.plot(x_inner, basis[i], alpha=0.1)
    
 # Initialize hamiltonian and S matrices
 T = np.zeros((N, N))
 A = np.zeros((N, N))
-E = 1
 
 for i in range(N):
     for j in range(N):
         # calculate Tij
-        integrand1 = -dB(x_inner, k, i, t, c) * dB(x_inner, k, j, t, c) + 2 * B(x_inner, k, i, t, c) * (E - V(x_inner)) * B(x_inner, k, j, t, c)
+        integrand1 = -dbasis[i] * dbasis[j] + 2 * basis[i] * (E - V(x_inner)) * basis[j]
         T[i, j] = integrate.simpson(integrand1, x=x_inner)
-            
         # calculate Aij
-        A[i, j] = B(x_m, k, i, t, c) * B(x_m, k, j, t, c)
+        A[i, j] = B(i, x_m) * B(j, x_m)
 
 eigvals, eigvecs = la.eig(T, A)
 eigvals = eigvals.real
@@ -77,7 +97,7 @@ print(eigvals)
 
 idx = 0
 for i, val in enumerate(eigvals):
-    if val != float('inf'):
+    if -float('inf') < val < float('inf'):
         idx = i
 
 b = eigvals[idx]
@@ -85,7 +105,7 @@ cs = eigvecs[:, idx]
 
 print("Log Derivative (-b): ", -b)
 
-# calculate wavefucntion from bspline
+# calculate wavefunction from bspline
 def psi(x, c):
     spl = BSpline(t, c, k)
     return spl(x)
@@ -97,10 +117,12 @@ even_shift = np.arctan(b / k) - (k * x_m)
 A = inner_psi[-1] / np.cos(k * x_m + even_shift)
 outer_psi = A * np.cos([k * x + even_shift for x in x_outer])
 
+
 plt.plot(x_inner, inner_psi, label='ψ(x)', linewidth=1.5)
 plt.plot(x_outer, outer_psi, label='ψ(x)', linewidth=1.5)
 plt.plot(x_full, V(x_full), label="Gaussian Potential", color="black", linewidth=2)
 
+plt.gca().set_aspect(10, adjustable='datalim')  # Ratio of y-unit to x-unit
 plt.axhline(0, color='black', linewidth=1)  
 plt.title("1D Scattering Wavefunction")
 plt.xlabel("Position x")
--- BSpline.py
+++ BSpline.py
@@ -16,9 +16,13 @@ import matplotlib.pyplot as plt
 #    return c1 + c2
 
 # Number of basis functions
-n = 25
+n = 30 - 1
+
 # Order
 k = 5
+
+N = n + k + 1
+
 # Knot set
 t = []
 for _ in range(k):
@@ -27,30 +31,26 @@ for i in range(n - k + 1):
     t.append(i)
 for _ in range(k):
     t.append(n - k)
+   
+print(t)
 
 # Coefficient array
-c = [0 for _ in range(n)]
-spl = BSpline(t, c, k)
+c = [0 for _ in range(n+1)]
 x = np.linspace(0, n - k, 10000)
 
-plt.figure(figsize=(9, 9))
-
-# x array, order, id, knot set, coeff arr
-def B(x, k, i, t, c):
+# id, x array 
+def B(i, x):
    c[i] = 1
    spl = BSpline(t, c, k)
    c[i] = 0
    return spl(x)
 
+y_0 = 0.4
+basis = [ y_0 * (B(0, x) + B(1, x)) ] + [ B(i, x) for i in range(2, n+1)]
+
+plt.figure(figsize=(9, 9))
 for i in range(n):
-   plt.plot(x, B(x, k, i, t, c))
-   
-# assert (n >= k+1) and (len(c) >= n)
-# for i in range(n):
-#     b = np.zeros_like(x)
-#     for j in range(len(x)):
-#         b[j] = c[i] * B(x[j], k, i, t)
-#     plt.plot(x, b)
+    plt.plot(x, basis[i])
 
 plt.title("BSpliney")
 plt.xlabel("x")
